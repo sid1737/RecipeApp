198506b767febf8698755efd117e594262fd3397
Minato_1737 <sidhdeshvadgaonkar@outlook.com>
2025-03-06 00:29:07 +1100

    refactor: uplifted the code in RecipeRepositoryImpl

--- app/src/main/java/com/example/recipeapp/data/repository/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/example/recipeapp/data/repository/RecipeRepositoryImpl.kt
@@ -3,14 +3,14 @@ package com.example.recipeapp.data.repository
 import android.content.Context
 import coil.request.Options
 import com.example.recipeapp.data.mapper.RecipeMapper
-import com.example.recipeapp.data.model.RecipeEntity
+import com.example.recipeapp.data.model.RecipeEntityList
 import com.example.recipeapp.dispatchers.AppDispatchers
 import com.example.recipeapp.domain.models.Recipe
 import com.example.recipeapp.domain.repository.RecipeRepository
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.withContext
+import java.io.InputStreamReader
 import javax.inject.Inject
 
 class RecipeRepositoryImpl @Inject constructor(
@@ -22,14 +22,10 @@ class RecipeRepositoryImpl @Inject constructor(
     override suspend fun getRecipe(): List<Recipe> {
         return withContext(appDispatchers.io) {
             try {
-                val jsonString = context.assets.open("data/recipesSample.json")
-                    .bufferedReader()
-                    .use { it.readText() }
-                val recipeEntityType = object : TypeToken<List<RecipeEntity>>() {}.type
-                val listOfRecipeEntities = gson.fromJson<List<RecipeEntity>>(jsonString, recipeEntityType)
-                listOfRecipeEntities.map {
-                    recipeMapper.map(it, Options(context))
-                }
+                val jsonString = context.assets.open("recipesSample.json")
+                val inputStream = InputStreamReader(jsonString)
+                val listOfRecipes = gson.fromJson(inputStream, RecipeEntityList::class.java)
+                recipeMapper.map(listOfRecipes, Options(context))
             } catch (exception: Exception) {
                 throw exception
             }
