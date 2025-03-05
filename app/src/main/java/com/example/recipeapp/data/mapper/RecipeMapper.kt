ec8717523c375dacfff10b831eee2d7b7f0cb047
Minato_1737 <sidhdeshvadgaonkar@outlook.com>
2025-03-06 00:24:13 +1100

    fix: update RecipeMapper to correctly map RecipeEntityList to List<Recipe>

--- app/src/main/java/com/example/recipeapp/data/mapper/RecipeMapper.kt
+++ app/src/main/java/com/example/recipeapp/data/mapper/RecipeMapper.kt
@@ -4,7 +4,7 @@ import coil.map.Mapper
 import coil.request.Options
 import com.example.recipeapp.data.model.IngredientEntity
 import com.example.recipeapp.data.model.RecipeDetailsEntity
-import com.example.recipeapp.data.model.RecipeEntity
+import com.example.recipeapp.data.model.RecipeEntityList
 import com.example.recipeapp.domain.models.Ingredient
 import com.example.recipeapp.domain.models.Recipe
 import com.example.recipeapp.domain.models.RecipeDetails
@@ -12,22 +12,24 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class RecipeMapper @Inject constructor() : Mapper<RecipeEntity, Recipe> {
-    override fun map(data: RecipeEntity, options: Options): Recipe {
-        return Recipe(
-            dynamicDescription = data.dynamicDescription,
-            dynamicThumbnail = data.dynamicThumbnail,
-            dynamicThumbnailAlt = data.dynamicThumbnailAlt,
-            dynamicTitle = data.dynamicTitle,
-            ingredients = mapIngredientEntityToIngredient(data.ingredients),
-            recipeDetails = mapRecipeDetailsEntityToRecipeDetails(data.recipeDetails)
-        )
+class RecipeMapper @Inject constructor() : Mapper<RecipeEntityList, List<Recipe>> {
+    override fun map(data: RecipeEntityList, options: Options): List<Recipe> {
+        return data.recipes.map { recipe ->
+            Recipe(
+                dynamicDescription = recipe.dynamicDescription,
+                dynamicThumbnail = recipe.dynamicThumbnail,
+                dynamicThumbnailAlt = recipe.dynamicThumbnailAlt,
+                dynamicTitle = recipe.dynamicTitle,
+                ingredients = mapIngredientEntityToIngredient(recipe.ingredients),
+                recipeDetails = mapRecipeDetailsEntityToRecipeDetails(recipe.recipeDetails)
+            )
+        }
     }
 
-    private fun mapIngredientEntityToIngredient(ingredientEntity: List<IngredientEntity>): List<Ingredient> {
-        return ingredientEntity.map {
+    private fun mapIngredientEntityToIngredient(ingredientEntityList: List<IngredientEntity>): List<Ingredient> {
+        return ingredientEntityList.map { ingredientEntity ->
             Ingredient(
-                ingredient = it.ingredient
+                ingredient = ingredientEntity.ingredient
             )
         }
     }
