90bf22e8659b59a401d9027dd9ad5a74a67c9174
Minato_1737 <sidhdeshvadgaonkar@outlook.com>
2025-03-09 04:19:10 +1100

    feat: create Navigation for the App

---
+++ app/src/main/java/com/example/recipeapp/ui/navigation/RecipeAppNavigation.kt
@@ -0,0 +1,40 @@
+package com.example.recipeapp.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import com.example.recipeapp.domain.models.Recipe
+import com.example.recipeapp.ui.recipeDetailsScreen.RecipeDetailScreen
+import com.example.recipeapp.ui.recipeListScreen.RecipeListScreen
+import kotlin.reflect.typeOf
+
+@Composable
+fun RecipeAppNavigation(
+    modifier: Modifier,
+    navController: NavHostController
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = ScreenNames.RecipeListScreen
+    ) {
+        composable<ScreenNames.RecipeListScreen> {
+            RecipeListScreen(
+                onRecipeClick = { recipe ->
+                    navController.navigate(ScreenNames.RecipeDetailScreen(recipe))
+                }
+            )
+        }
+        composable<ScreenNames.RecipeDetailScreen> (
+            typeMap = mapOf(
+                typeOf<Recipe>() to CustomNavType.RecipeType
+            )
+        ) {
+            val arguments = it.toRoute<ScreenNames.RecipeDetailScreen>()
+            RecipeDetailScreen(recipe = arguments.recipe)
+        }
+    }
+}
