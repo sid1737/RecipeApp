595ef9e2fb75619c060ae244ea264ad6e89d385f
Minato_1737 <sidhdeshvadgaonkar@outlook.com>
2025-03-09 04:14:51 +1100

    refactor: move Loading Indicator to the common components under ui directory

--- app/src/main/java/com/example/recipeapp/ui/commonComponents/LoadingIndicator.kt
+++ app/src/main/java/com/example/recipeapp/ui/commonComponents/LoadingIndicator.kt
@@ -1,4 +1,4 @@
-package com.example.recipeapp.ui.recipeListScreen.components
+package com.example.recipeapp.ui.commonComponents
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
