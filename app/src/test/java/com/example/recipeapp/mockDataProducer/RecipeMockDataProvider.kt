f6aa2bf009fbab6426ec2958b7a8801623ee1c3c
Minato_1737 <sidhdeshvadgaonkar@outlook.com>
2025-03-09 19:20:51 +1100

    feat: create RecipeMockDataProvider for providing mock data for Recipe

---
+++ app/src/test/java/com/example/recipeapp/mockDataProducer/RecipeMockDataProvider.kt
@@ -0,0 +1,68 @@
+package com.example.recipeapp.mockDataProducer
+
+import com.example.recipeapp.data.model.IngredientEntity
+import com.example.recipeapp.data.model.RecipeDetailsEntity
+import com.example.recipeapp.data.model.RecipeEntity
+import com.example.recipeapp.data.model.RecipeEntityList
+import com.example.recipeapp.domain.models.Ingredient
+import com.example.recipeapp.domain.models.Recipe
+import com.example.recipeapp.domain.models.RecipeDetails
+
+object RecipeMockDataProvider {
+
+    fun getMockRecipeEntityList(): RecipeEntityList {
+        return RecipeEntityList(
+            recipes = listOf(
+                RecipeEntity(
+                    dynamicDescription = "dynamic description",
+                    dynamicThumbnail = "dynamic thumbnail",
+                    dynamicThumbnailAlt = "dynamic thumbnail alt",
+                    dynamicTitle = "dynamic title",
+                    ingredients = listOf(
+                        IngredientEntity(
+                            ingredient = "ingredient"
+                        )
+                    ),
+                    recipeDetails = RecipeDetailsEntity(
+                        amountLabel = "amount label",
+                        amountNumber = 9,
+                        cookTimeAsMinutes = 120,
+                        cookingLabel = "cooking label",
+                        cookingTime = "4h 20m",
+                        prepLabel = "prep label",
+                        prepNote = "prep note",
+                        prepTime = "20m",
+                        prepTimeAsMinutes = 40
+                    )
+                )
+            )
+        )
+    }
+
+    fun getMockRecipeList(): List<Recipe> {
+        return listOf(
+            Recipe(
+                dynamicDescription = "dynamic description",
+                dynamicThumbnail = "dynamic thumbnail",
+                dynamicThumbnailAlt = "dynamic thumbnail alt",
+                dynamicTitle = "dynamic title",
+                ingredients = listOf(
+                    Ingredient(
+                        ingredient = "ingredient"
+                    )
+                ),
+                recipeDetails = RecipeDetails(
+                    amountLabel = "amount label",
+                    amountNumber = 9,
+                    cookTimeAsMinutes = 120,
+                    cookingLabel = "cooking label",
+                    cookingTime = "4h 20m",
+                    prepLabel = "prep label",
+                    prepNote = "prep note",
+                    prepTime = "20m",
+                    prepTimeAsMinutes = 40
+                )
+            )
+        )
+    }
+}
