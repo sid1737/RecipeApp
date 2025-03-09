d93ac78c33176ef254461c620408f8b41bf04f06
Minato_1737 <sidhdeshvadgaonkar@outlook.com>
2025-03-09 16:17:23 +1100

    refactor: remove serializable annotation from domain object

--- app/src/main/java/com/example/recipeapp/domain/models/Ingredient.kt
+++ app/src/main/java/com/example/recipeapp/domain/models/Ingredient.kt
@@ -1,8 +1,5 @@
 package com.example.recipeapp.domain.models
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class Ingredient(
     val ingredient: String
 )
--- app/src/main/java/com/example/recipeapp/domain/models/Recipe.kt
+++ app/src/main/java/com/example/recipeapp/domain/models/Recipe.kt
@@ -1,11 +1,8 @@
 package com.example.recipeapp.domain.models
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class Recipe(
     val dynamicDescription: String,
-    val dynamicThumbnail: String,
+    var dynamicThumbnail: String,
     val dynamicThumbnailAlt: String,
     val dynamicTitle: String,
     val ingredients: List<Ingredient>,
