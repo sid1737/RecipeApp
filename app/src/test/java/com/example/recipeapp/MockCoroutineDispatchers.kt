1c3c6c7fde4369bd25da19cda79f5dd4f5c18aac
Minato_1737 <sidhdeshvadgaonkar@outlook.com>
2025-03-10 12:53:14 +1100

    refactor: removed the UI dispatcher from App Dispatcher

--- app/src/test/java/com/example/recipeapp/MockCoroutineDispatchers.kt
+++ app/src/test/java/com/example/recipeapp/MockCoroutineDispatchers.kt
@@ -6,6 +6,5 @@ import kotlinx.coroutines.test.StandardTestDispatcher
 
 class MockCoroutineDispatchers: AppDispatchers {
     private val standardTestDispatcher = StandardTestDispatcher()
-    override val ui: CoroutineDispatcher = standardTestDispatcher
     override val io: CoroutineDispatcher = standardTestDispatcher
 }
